d07fdee9520dc80d71784075358281782313d537
Andrew Hickman <ahickma4@students.rcgc.edu>
2021-05-05 15:16:47 -0400

    Add comments, reset change quote refresh timer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 /******************************************
 Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+Project 1 - A Random Quote Generator
 ******************************************/
 
 /***
  * `getRandomQuote` function
+ * return a random quote object from an array of quote objects
 ***/
 
 const getRandomQuote = (quotes) => {
@@ -12,17 +13,22 @@ const getRandomQuote = (quotes) => {
     return quotes[randomQuoteIndex];
 }
 
+/***
+ * `getRandomBackground` function
+ * generates and displays a random background color on page when called
+ */
+
 const generateRandomBackground = () => {
-
     const randomRGBValue = () => {
-
+        // Return a string with a properly formatted RGB value
         const number = () => {
+            // Return a random number between 0 and 255
             return Math.floor(Math.random() * 255);
         }
 
         return `rgb(${number()}, ${number()}, ${number()})`;
     }
-
+    // Display the random color
     document.body.style.backgroundColor = randomRGBValue();
 }
 
@@ -30,41 +36,53 @@ const generateRandomBackground = () => {
 
 /***
  * `printQuote` function
+ * builds an html template using the data from a random quote
+ * displays quote on page
+ * returns the quote and html template for use in other functions if needed
 ***/
 
 const printQuote = randomQuote => {
     const quote = randomQuote;
 
+    // Build html template from quote object data
     let html = ``;
     html += `<p class="quote">${quote.quote}</p>`;
     html += `<p class="source">${quote.source}`;
-
+    // Add if translation from foreign language is provided
     if (quote.translation) {
         html += `<p>Translation: ${quote.translation}</p>`;
     }
-
+    // Add if citation is provided
     if (quote.citation){
         html += `<span class="citation">${quote.citation}</span>`;
     }
-
+    // Add if year is provided
     if (quote.year) {
         html += `<span class="year">${quote.year}</span>`;
     }
 
     html += `</p>`;
 
+    // Display quote on page
     document.getElementById('quote-box').innerHTML = html;
+    // Display random background color with quote
     generateRandomBackground();
-
+    // Returns the template and random quote if needed
     return html;
 }
 
-
+// Initially display random quote on page load
 document.getElementById('quote-box').innerHTML = printQuote(getRandomQuote(quotes));
-setInterval(() => printQuote(getRandomQuote(quotes)), 5000);
+// Change quote automatically after 15 seconds
+let displayTimer = setInterval(() => printQuote(getRandomQuote(quotes)), 5000);
 
 /***
  * click event listener for the print quote button
 ***/
 
-document.getElementById('load-quote').addEventListener("click",() => printQuote(getRandomQuote(quotes)), false);
+document.getElementById('load-quote').addEventListener("click",() => {
+    printQuote(getRandomQuote(quotes));
+    // Reset the quote refresh timer if change quote button is pressed
+    clearInterval(displayTimer);
+    displayTimer = setInterval(() => printQuote(getRandomQuote(quotes)), 5000)
+}, false);
